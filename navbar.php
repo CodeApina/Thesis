81a15f2a87bac68a098a59a455a257616a395de5
Henri Sarén <henri.saren@gmail.com>
2023-02-04 22:34:46 +0200

    Reverted index.php & navbar.php to previous commit

---
+++ navbar.php
@@ -0,0 +1,34 @@
+<?php
+include "backend/bootstrap.php"; ?>
+<html>
+    <nav class="nav navbar-nav navbar-expand-sm" style="background-color:#8E7970; height: 3%">
+        <div class="container-fluid">
+            <a class="navbar-brand" style="color:#F55449" href=""><b>HHK</b></a>
+        </div>
+    </nav>
+    <div class=row style="max-width: 300px; height: 100%">
+        <nav class="nav navbar-nav fixed-left ms-1" style="background-color: #8E7970">
+            <div class="container-fluid">
+                <form class="d-flex">
+                    <span>
+                        <input class="form-control me-1" type="text" placeholder="Search"><br>
+                    </span>
+                    <span>
+                        <button style="background-color: #0F1F38; color: #F55449" class="btn" type="button">Search</button>
+                    </span>
+                </form>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a id="home_link" class="nav-link" href="">asdasd</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="" href=""></a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="wishlist" class="nav-link" href="">asdasda</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</html>
